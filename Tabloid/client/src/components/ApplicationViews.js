33a533811a304857f3d9a0325ede6516753aa178
abbeyroyse13 <heatherruan126@gmail.com>
2021-10-13 13:42:13 -0500

    group code; ability to view current user's posts complete

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import PostList from "./Post/PostList";
+import UserPostList from "./Post/UserPostList";
 
 export default function ApplicationViews({ isLoggedIn }) {
 
@@ -13,6 +14,9 @@ export default function ApplicationViews({ isLoggedIn }) {
           {isLoggedIn ? <PostList /> : <Redirect to="/login" />}
         </Route>
 
+        <Route path="/myPosts">
+          {isLoggedIn ? <UserPostList /> : <Redirect to="/login" />}
+        </Route>
 
         <Route path="/login">
           <Login />
