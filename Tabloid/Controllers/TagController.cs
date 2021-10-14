3773b30e95182c87dbf1c40b50bb43106e541cce
Kipp Minton <luciankippminton@gmail.com>
2021-10-14 13:50:08 -0500

    tag class, repo, Irepo and controllers created with methods for retrieving tag list

---
+++ Tabloid/Controllers/TagController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Repositories;
+
+namespace Tabloid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly ITagRepository _tagRepository;
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        //https://localhost:5001/api/tag/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var tags = _tagRepository.GetAllTags();
+            return Ok(tags);
+        }
+    }
+}
